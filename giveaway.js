b26a85cc01b086a37f95854d6d269cb8b832579b
Mike Thomas <mike_thomas@yahoo.com>
2015-10-31 16:43:44 -0600

    Minimize options

--- giveaway.js
+++ giveaway.js
@@ -69,30 +69,9 @@ var renderQuest = function (apigClient) {
 };
 
 $(window).load(function(){
-    var opts = {
-      lines: 13 // The number of lines to draw
-    , length: 28 // The length of each line
-    , width: 14 // The line thickness
-    , radius: 42 // The radius of the inner circle
-    , scale: .75 // Scales overall size of the spinner
-    , corners: 1 // Corner roundness (0..1)
-    , color: '#000' // #rgb or #rrggbb or array of colors
-    , opacity: 0.25 // Opacity of the lines
-    , rotate: 0 // The rotation offset
-    , direction: 1 // 1: clockwise, -1: counterclockwise
-    , speed: 0.9 // Rounds per second
-    , trail: 48 // Afterglow percentage
-    , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-    , zIndex: 2e9 // The z-index (defaults to 2000000000)
-    , className: 'spinner' // The CSS class to assign to the spinner
-    , top: '25%' // Top position relative to parent
-    , left: '50%' // Left position relative to parent
-    , shadow: false // Whether to render a shadow
-    , hwaccel: false // Whether to use hardware acceleration
-    , position: 'absolute' // Element positioning
-    }
+    var spinnerOpts = { scale: 1.5, top: '25%', left: '50%', position: 'absolute' }
     var target = document.getElementById('body')
-    var spinner = new Spinner(opts).spin(target);
+    var spinner = new Spinner(spinnerOpts).spin(target);
     
     var apigClient = apigClientFactory.newClient();
 
